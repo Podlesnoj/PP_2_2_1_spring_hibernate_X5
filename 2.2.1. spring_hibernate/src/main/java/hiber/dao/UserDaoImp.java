b47749c9ddc1942d03a330e0d1dedd23fa1716cc
Podlesnoj <motmos11@gmail.com>
2025-10-25 15:16:38 +0300

    Update UserDaoImp.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+import java.util.Optional;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,33 +12,38 @@ import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
-
     @Autowired
     private SessionFactory sessionFactory;
 
+
+
     @Override
     public void add(User user) {
+        if (user.getCar() != null) {
+            sessionFactory.getCurrentSession().save(user.getCar());
+        }
         sessionFactory.getCurrentSession().save(user);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("from User u join fetch u.car", User.class);
         return query.getResultList();
     }
 
     @Override
     @SuppressWarnings("unchecked")
-    public User getUserByCar(String model, int series) {
+    public Optional<User> getUserByCar(String model, int series) {
         try {
             TypedQuery<User> query = sessionFactory.getCurrentSession()
-                    .createQuery("from User user where user.car.model = :model and user.car.series = :series", User.class);
+                    .createQuery("from User u join fetch u.car where u.car.model = :model and u.car.series = :series", User.class);
             query.setParameter("model", model);
             query.setParameter("series", series);
-            return query.setMaxResults(1).getSingleResult();
+            return Optional.ofNullable(query.setMaxResults(1).getSingleResult());
         } catch (NoResultException e) {
-            return null;
+            return Optional.empty();
         }
     }
 }
